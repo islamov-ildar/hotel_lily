0f774897f151b05499886996fe87d71984a9d5ce
islamov-ildar <islamov.ildar@gmail.com>
2024-07-11 23:24:38 +0300

    add data to feedbacks

--- src/components/FeedBackCard.vue
+++ src/components/FeedBackCard.vue
@@ -1,13 +1,35 @@
 <script lang="ts">
+import {onMounted, ref} from "vue";
+
 export default {
   props: ['number', 'data'],
+
+  setup(props) {
+
+    const rowCount = ref(0);
+
+    console.log('props.number', props.number)
+
+    onMounted(() => {
+      // const div = document.getElementById(`feedback_text-${props.number}`)!.offsetHeight;
+      //
+      //
+      rowCount.value = document.getElementById(`feedback_text-${props.number}`)!.offsetHeight;
+    })
+
+    return {
+      rowCount
+    }
+  }
 }
 </script>
 
 <template>
   <div class="card relative">
-    <div class="absolute top-[-40px] left-[55px]">
-      <img v-if="data.user_photo !== 'default'" :src="`src/assets/images/users/${data.user_photo}`" alt="person_elips">
+    <div class="absolute top-[-60px] left-[55px] w-[132px] h-[132px] rounded-t-full bg-[#FFFFFF] flex items-center justify-center">
+      <div v-if="data.user_photo !== 'default'" class="w-[90px] h-[90px] rounded-full overflow-hidden">
+        <img :src="`src/assets/images/users/${data.user_photo}`" alt="person_elips">
+      </div>
       <img v-else src="@/assets/icons/person_elips.svg" alt="person_elips">
     </div>
     <div class="w-full text-left pt-[30px] pb-[30px] px-[30px] text-blueMain">
@@ -23,10 +45,10 @@ export default {
         </div>
         <div>{{ data.feedback_date }}</div>
       </div>
-      <div class="font-montserratRegular text-[16px] leading-[22px]">
-        Отдых на все 100 %!!! Полный релакс! Заселение за 3 минуты в современный, оснащённый всем необходимым номер, уборка каждый день, вкусные разнообразные завтраки. Сама гостиница находится в 2х минутах от моря ...
+      <div :id="`feedback_text-${number}`" class="feedbackText font-montserratRegular text-[16px] leading-[22px]">
+        {{ data.feedback_text }}
       </div>
-      <div class="mt-[8px] cursor-pointer font-montserratMedium text-[16px] flex items-center gap-[7px]">
+      <div v-if="rowCount === 88" class="mt-[8px] cursor-pointer font-montserratMedium text-[16px] flex items-center gap-[7px]">
         <div>Читать отзыв</div><img src="@/assets/icons/arrow-right_blue.svg" alt="arrow-right_blue">
       </div>
     </div>
@@ -38,5 +60,19 @@ export default {
   width: 682px;
   height: 310px;
   background-color: #FFFFFF;
+}
+.feedbackText{
+  -ms-text-overflow: ellipsis;
+  -o-text-overflow: ellipsis;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  -ms-line-clamp: 4;
+  -webkit-line-clamp: 4;
+  line-clamp: 4;
+  display: -webkit-box;
+  display: box;
+  word-wrap: break-word;
+  -webkit-box-orient: vertical;
+  box-orient: vertical;
 }
 </style>
